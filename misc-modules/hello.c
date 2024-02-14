a8c04ed42b59d3a78897420992fd1f1aedab8445
Mohsin Sardar <mohsin.sardar@seecs.edu.pk>
2024-02-14 12:09:09 +0500

    Hello file in misc-modules upadated.

--- misc-modules/hello.c
+++ misc-modules/hello.c
@@ -7,13 +7,13 @@ MODULE_LICENSE("Dual BSD/GPL");
 
 static int hello_init(void)
 {
-	printk(KERN_ALERT "Hello, world\n");
+	printk(KERN_ALERT "Hello, world, mohsinsardar\n");
 	return 0;
 }
 
 static void hello_exit(void)
 {
-	printk(KERN_ALERT "Goodbye, cruel world\n");
+	printk(KERN_ALERT "Goodbye, cruel world, mohsinsardar\n");
 }
 
 module_init(hello_init);
